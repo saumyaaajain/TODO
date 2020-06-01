652a878a76f71647b4800b0835443c2d9df536fc
saumya <jsaumya988@gmail.com>
2020-06-02 03:32:01 +0530

    added animations to add task page as well

--- src/components/AddTaskPage.js
+++ src/components/AddTaskPage.js
@@ -6,6 +6,7 @@ import '../style/ViewFile.css';
 import Title from "./Title";
 import {useStyles} from "../style/AddTaskPage";
 import Paper from "@material-ui/core/Paper";
+import Grow from "@material-ui/core/Grow";
 
 
 const AddTaskPage = (props) => {
@@ -13,18 +14,24 @@ const AddTaskPage = (props) => {
     props.getTitle('ADD TASK LIST');
     return (
     <React.Fragment>
-        <Paper className={classes.paper}>
-            <Title>
-                Add Task List
-            </Title>
-            <AddFile
-                onSubmit = { (taskList) => {
-                    console.log(taskList);
-                    props.dispatch(addTaskList(taskList));
-                    props.location && props.history.push('/');
-                }}
-            />
-        </Paper>
+        <Grow
+            in={true}
+            style={{ transformOrigin: '0 0 0' }}
+            {...{timeout: 5000}}
+        >
+            <Paper className={classes.paper}>
+                <Title>
+                    Add Task List
+                </Title>
+                <AddFile
+                    onSubmit = { (taskList) => {
+                        console.log(taskList);
+                        props.dispatch(addTaskList(taskList));
+                        props.location && props.history.push('/');
+                    }}
+                />
+            </Paper>
+        </Grow>
     </React.Fragment>
 )};
 
